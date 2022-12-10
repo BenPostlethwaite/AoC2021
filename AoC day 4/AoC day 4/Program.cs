c2e316b2cf1763cda3d3446e526897a5105f285a
Ben Postlethwaite <Ben.Postlethwaite05@gmail.com>
2022-12-10 14:32:04 +0000

    first commit

---
+++ AoC day 4/AoC day 4/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace AoC_day_4
+{
+    class number
+    {
+        public int value;
+        public bool marked = false;
+        public number()
+        {
+            marked = false;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] textFile = File.ReadAllLines("test.txt");
+            List<string> data = new List<string>();
+
+            int cardSize = textFile[2].Split(' ').Count();
+
+            for(int line = 2; line <= textFile.Length-cardSize+2; line++)
+            {
+                List<List<string>> bingoCard = new List<List<string>>();
+
+
+                for (int j = 0; j < cardSize; j++)
+                {
+                    List<string> cardLine = new List<string>();
+
+                    string[] cardLineArray = textFile[line].Split(' ');
+                    foreach (string value in cardLineArray)
+                    {
+                        cardLine.Add(value);
+                    }
+                    bingoCard.Add(cardLine);
+                }
+            }
+
+
+
+            string[] numsToDraw = data[0].Split(',');
+            int cardNum = 1;
+            int index = 0;
+
+
+
+            /*
+            while (index < cardNum*(cardSize+1))
+            {
+
+            }
+            for (int i = 0; i < cardSize; i++)
+            {
+
+            }
+            */
+        }
+    }
+}
---
+++ advent of code day 3/advent of code day 3/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace advent_of_code_day_3
+{
+    internal class Program
+    {
+        static bool CountOnes(List<string> dataList, int i)
+        {
+            bool moreOnes;
+            int ones = 0;
+            int zeros = 0;
+
+            for (int j = 0; j < dataList.Count; j++)
+            {
+                if (dataList[j][i] == '1')
+                {
+                    ones += 1;
+                }
+                else
+                {
+                    zeros += 1;
+                }
+            }
+            if (ones > zeros)
+            {
+                moreOnes = true;
+            }
+            else if (ones < zeros)
+            {
+                moreOnes = false;
+            }
+            else
+            {
+                moreOnes = true;
+            }
+            
+            return moreOnes;
+        }
+
+        static void Main(string[] args)
+        {
+            //make dataList
+
+            List<string> dataList = new List<string>();
+            string[] dataArray = File.ReadAllLines("input.txt");
+            for (int j = 0; j < dataArray.Length; j++)
+            {
+                dataList.Add(dataArray[j]);
+            }
+
+            //iterate untill only one number left
+
+            int i = 0;
+            while (dataList.Count > 1)
+            {
+                bool moreOnes = CountOnes(dataList, i);
+
+                    //remove half of list
+                    for (int j = dataList.Count-1; j >= 0; j--)
+                    {
+                        if (moreOnes)
+                        {
+                            if (dataList[j][i] == '1')
+                            {
+                                dataList.RemoveAt(j);
+                            }
+                        }
+                        else
+                        {
+                            if (dataList[j][i] == '0')
+                            {
+                                dataList.RemoveAt(j);
+                            }
+                        }
+                    }
+                    foreach (string data in dataList)
+                    {
+                        Console.WriteLine(data);
+                    }
+                    Console.WriteLine("\n");                
+                i++;
+            }
+            Console.ReadKey();
+        }
+    }
+}
